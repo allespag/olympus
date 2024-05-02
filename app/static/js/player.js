b9e525d4cd15ed0e9f2d645a9b1cb0f4adc542b0
Alexandre <allespag@student.42.fr>
2024-05-03 00:33:14 +0200

    feat(player frontend): audio, buttons, citation done

--- app/static/js/player.js
+++ app/static/js/player.js
@@ -1,10 +1,28 @@
+function showPlaying() {
+  var playButton = document.getElementById("icon-button-play");
+  var stopButton = document.getElementById("icon-button-stop");
+
+  playButton.style.display = "none";
+  stopButton.style.display = "inline-block";
+}
+
+function showStopped() {
+  var playButton = document.getElementById("icon-button-play");
+  var stopButton = document.getElementById("icon-button-stop");
+
+  playButton.style.display = "inline-block";
+  stopButton.style.display = "none";
+}
+
 function toggleAudio() {
   var audio = document.getElementById("tag-audio");
+
+  // TODO: create a slide volume
+  audio.volume = 0.5;
+
   if (audio.paused) {
     audio.play();
-    document.querySelector(".audio-button").innerText = "Pause Audio";
   } else {
     audio.pause();
-    document.querySelector(".audio-button").innerText = "Play Audio";
   }
 }
