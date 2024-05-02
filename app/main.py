f5a5f58ade406c55ff3a37cd8d6225197b6210b7
Alexandre <allespag@student.42.fr>
2024-05-02 21:29:03 +0200

    feat(soundboard): add favicon

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
+from pathlib import Path
 from random import choice
 
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+STATIC_FOLDER_NAME = "static"
+STATIC_FOLDER = Path(f"./{STATIC_FOLDER_NAME}")
+
+app.mount(
+    f"/{STATIC_FOLDER_NAME}",
+    StaticFiles(directory=STATIC_FOLDER_NAME),
+    name=STATIC_FOLDER_NAME,
+)
 
 
 templates = Jinja2Templates(directory="templates")
@@ -51,6 +60,11 @@ async def tag(resquest: Request, tag_name: str):
     return templates.TemplateResponse(
         request=resquest, name="tag.html", context={"tags": TAGS, "tag": TAGS[tag_name]}
     )
+
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(STATIC_FOLDER / "images" / "favicon.ico")
 
 
 if __name__ == "__main__":
