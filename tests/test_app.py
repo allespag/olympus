6d066f42d0d2c6b14d8c7e81321efbfc66cd96aa
Alexandre <allespag@student.42.fr>
2024-05-05 23:52:42 +0200

    tests(app): add basic tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,3 @@
-import pytest
 from fastapi.testclient import TestClient
 
 from olympus.main import app
@@ -9,3 +8,8 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_on_404():
+    response = client.get("/this/route/does/not/exist")
+    assert response.url.path == "/"
